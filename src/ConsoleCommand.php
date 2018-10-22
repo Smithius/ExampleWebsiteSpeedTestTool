092d06f4efd902051f2d9101dd291c78b5ef8e69
Adrian Adamski <adrian.s.adamski@gmail.com>
2018-10-22 04:29:14 +0200

    Refactoring ConsoleCommand

--- src/ConsoleCommand.php
+++ src/ConsoleCommand.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 
 use Benchmarks\Measurer;
+use Entities\Measurement;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -8,6 +9,17 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class ConsoleCommand extends Command
 {
+    /**
+     * @var Measurer
+     */
+    private $measurer;
+
+    public function __construct(?string $name = null)
+    {
+        parent::__construct($name);
+        $this->measurer = new Measurer();
+    }
+
     protected function configure()
     {
         $this
@@ -20,20 +32,35 @@ class ConsoleCommand extends Command
         $websiteUrl = $input->getArgument('websiteUrl');
         $otherWebsitesUrls = $this->parseCompUrls($input->getArgument('otherWebsitesUrls'));
 
-        $measurer = new Measurer();
-        $testedWebsite = $measurer->measure($websiteUrl);
+        $testedWebsite = $this->measureWebsite($websiteUrl);
+
+        if (!$testedWebsite) {
+            throw new Exception('Can\'t measure tested website');
+        }
 
         $comparedWebsites = [];
         foreach ($otherWebsitesUrls as $url) {
-            $measurement = $measurer->measure($url);
-            $measurement->setParentMeasurement($testedWebsite);
+            $measurement = $this->measureWebsite($url, $testedWebsite);
 
-            $comparedWebsites[] = $measurement;
+            if ($measurement) {
+                $comparedWebsites[] = $measurement;
+            }
         }
 
         $cm = new ComparisonManager($testedWebsite, $comparedWebsites);
         $cm->setOutput($output)
             ->run();
+    }
+
+    private function measureWebsite($websiteUrl, $parent = null): ?Measurement
+    {
+        $measurement = $this->measurer->measure($websiteUrl);
+
+        if ($measurement && $parent) {
+            $measurement->setParentMeasurement($parent);
+        }
+
+        return $measurement;
     }
 
     private function parseCompUrls(string $comparedUrls): array
